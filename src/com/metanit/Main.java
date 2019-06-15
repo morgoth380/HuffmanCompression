1bb26687862ce532ad690149cebac4f08dfb3776
morgoth380 <morgoth380@gmail.com>
2019-06-15 11:32:26 +0300

    huffman

---
+++ src/com/metanit/Main.java
@@ -0,0 +1,14 @@
+package com.metanit;
+
+import java.util.Scanner;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        try {
+            FileInputStream fstream = new FileInputStream("testFile.txt"); //Файл с исходным кодом
+        }
+    }
+}
