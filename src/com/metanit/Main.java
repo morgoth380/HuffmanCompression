372bbce8c3a2ee17d6ebf2d704ec7d63848bb7b1
morgoth380 <morgoth380@gmail.com>
2019-06-15 12:17:56 +0300

    added

--- src/com/metanit/Main.java
+++ src/com/metanit/Main.java
@@ -2,13 +2,25 @@ package com.metanit;
 
 import java.util.Scanner;
 import java.io.*;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	ArrayList <String> FileArray = new ArrayList<String>(); //контейнер для считываемых с сжимаемого файла строк
+
         try {
-            FileInputStream fstream = new FileInputStream("testFile.txt"); //Файл с исходным кодом
+            FileInputStream fstream = new FileInputStream("testFile.txt"); //сжимаемый файл
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+            String strLine;
+            int i = 0;
+            while ((strLine = br.readLine()) != null){ //!Читаем построчно файл
+                FileArray.add(strLine); //!Сохраняем считанную строку во временном массиве строк
+                i++; //после выхода из цикла i будет равно коли количеству считанных строк
+            }
+            System.out.println(i);
+        }catch(IOException ex){
+            System.out.println(ex.getMessage());
         }
     }
 }
